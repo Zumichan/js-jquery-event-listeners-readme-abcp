a464750113eb1fb521362651c3fc01359a1bd2c6
Azumi Callanan <azucm@hotmail.com>
2018-01-18 04:35:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,11 @@
 //define functions here
 function getIt(){
-  
+  $('p').on('click',function(){
+    alert "Hey!"
+  })
 }
 
-$('h1').on("eventname", function(){
-    //action you want taken
-});
+
 
 $(document).ready(function(){
 
